a22f070d82012dff512abae0666a6191d68ae8e6
Andrew Coleman <Andrew@lawn-128-61-127-169.lawn.gatech.edu>
2019-08-27 13:56:55 -0400

    changed to serve from dist

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const path = require("path");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "/public/beer.html"));
+  res.sendFile(path.join(__dirname, "/dist/beer.html"));
 });
 
 app.listen(PORT, function() {

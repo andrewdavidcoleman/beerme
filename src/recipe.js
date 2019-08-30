ea67d168a974b239effb7f7c9d7466021aadc2cb
Andrew Coleman <Andrew@lawn-128-61-127-169.lawn.gatech.edu>
2019-08-30 18:19:00 -0400

    Renamed recipe.js to Recipe.js

--- src/recipe.js
+++ src/recipe.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import API from './API.js'
+import React, { Component } from 'react';
+import API from './API.js';
 
 class Recipe extends React.Component {
 
